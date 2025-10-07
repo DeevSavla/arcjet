c8cabd18b0c6673255a3d27fce3efe4f8d64992d
Deev Savla <savla.deev@gmail.com>
2025-10-07 21:44:09 +0530

    install arcjet

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.listen(port , ()=>{
+    console.log(`Server running on port ${port}`)
+})
