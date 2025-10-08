c576b9395285536c9b228457a3626c48a5611711
Deev Savla <savla.deev@gmail.com>
2025-10-08 12:10:42 +0530

    add rate limiting feature

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import arcjet, { validateEmail } from "@arcjet/node";
+import arcjet, { validateEmail, tokenBucket } from "@arcjet/node";
 import express from "express";
 import session from "express-session";
 import { configDotenv } from "dotenv";
@@ -6,10 +6,8 @@ import { configDotenv } from "dotenv";
 configDotenv();
 
 const app = express();
-
 const port = 3000;
 
-//in memory
 const users = [];
 
 app.use(express.json());
@@ -28,13 +26,19 @@ const aj = arcjet({
     validateEmail({
       mode: "LIVE",
       deny: ["DISPOSABLE", "INVALID", "NO_MX_RECORDS"],
-      //MX Record is a type of DNS record that specifies which mail server is responsible for receiving emails for a domain, acting as the digital address for incoming mail
+    }),
+    tokenBucket({
+      mode: "LIVE", // Set to "DRY_RUN" to test without blocking
+      characteristics: ["userId"], // Track requests per user
+      refillRate: 5, // Tokens refilled every interval
+      interval: 30, // Interval (seconds)
+      capacity: 10, // Max tokens
     }),
   ],
 });
 
 const displayEmails = () => {
-  console.log("Registered user");
+  console.log("Registered users:");
   users.forEach((user) => console.log(user.email));
 };
 
@@ -43,25 +47,33 @@ app.post("/signup", async (req, res) => {
     const { email, password } = req.body;
 
     const decision = await aj.protect(req, {
+      userId:email,
       email,
+      requested: 5, // consumes 5 tokens per request
     });
-    console.log("Arcjet decision", decision);
+
+    console.log("Arcjet decision:", decision);
 
     if (decision.isDenied()) {
       res.writeHead(403, { "Content-Type": "application/json" });
-      return res.end(JSON.stringify({ error: "Forbidden" }));
+      return res.end(JSON.stringify({ error: "Forbidden",message:decision.conclusion }));
     }
-
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Hello World", email: req.body.email }));
 
     if (users.find((user) => user.email === email)) {
-      return res.status(400).json({
-        message: "User already exists",
-      });
+      return res.status(400).json({ message: "User already exists" });
     }
+
+    users.push({ email, password });
+
+    displayEmails();
+
+    return res.status(200).json({
+      message: "Signup successful",
+      email,
+    });
   } catch (error) {
-    console.log(error)
+    console.error("Signup error:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
